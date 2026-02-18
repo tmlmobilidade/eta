2fe8192c5d46855cef36012204dd40bf4f65c40f
jusimen <jusi.monteiro@protonmail.com>
2026-02-18 12:53:03 +0000

    Change log level from Info to Debug for event bearing check in ProcessLineShapes to reduce log verbosity while maintaining necessary information for debugging.

--- modules/2. segment-travel-times-go/src/processor/travel_times.go
+++ modules/2. segment-travel-times-go/src/processor/travel_times.go
@@ -63,7 +63,7 @@ func ProcessLineShapes(
 				nodesBearing := geo.CalculateBearing(prevEventNode, currEventNode)
 
 				if eventsBearing == 0 && prevEventNode != currEventNode{
-					lib.AppLogger.Info("Events bearing is 0 and nodes are different, skipping segment %d -> %d", slices.Index(shapeNodes, prevEventNode), slices.Index(shapeNodes, currEventNode))
+					lib.AppLogger.Debug("Events bearing is 0 and nodes are different, skipping segment %d -> %d", slices.Index(shapeNodes, prevEventNode), slices.Index(shapeNodes, currEventNode))
 					continue;
 				}
 
