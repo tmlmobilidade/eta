af847c4278420824fb3d2984cee281624ac7f561
jusimen <jusi.monteiro@protonmail.com>
2026-02-20 14:56:00 +0000

    Update line IDs in syncVehicleEvents to include specific development-only IDs for improved data filtering during synchronization.

--- modules/1. sync-vehicle-events/src/index.ts
+++ modules/1. sync-vehicle-events/src/index.ts
@@ -58,7 +58,7 @@ async function syncVehicleEvents(writer: ClickHouseWriter<EtaVehicleEvent>, star
 
 	const ridesQuery: Filter<Ride> = {
 		agency_id: { $in: AGENCY_IDS },
-		// line_id: { $in: [1001, 1002] }, // ! Development only
+		line_id: { $in: [2652, 2708, 2711, 2713, 2722, 2725, 2728, 2729, 2730, 2731, 2734] }, // ! Development only
 		start_time_observed: { $ne: null },
 		start_time_scheduled: { $gte: start.unix_timestamp, $lt: end.unix_timestamp },
 	};
